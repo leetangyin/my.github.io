8d1421c284ee15c44963a003277cdc4634a2f102
李棠寅 <379463001@qq.com>
2018-06-14 17:11:40 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 $('.carousel').carousel()
+
+//评论跳转
+function gotalklist(){
+    window.location.href = "talklist.html"
+}
+
+//跳转新闻页面
+function gonewClick(){
+    alert('lee')
+}
---
+++ js/news.js
@@ -0,0 +1,5 @@
+//返回上一级事件
+function goBackClick(){
+    window.history.go(-1);
+}
+
