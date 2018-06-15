5e7380e974cbbc34b846ecf30b88470c446f1aba
李棠寅 <379463001@qq.com>
2018-06-15 18:19:10 +0800

    Add files via upload

--- js/talklist.js
+++ js/talklist.js
@@ -3,6 +3,18 @@ function goBackClick(){
     window.history.go(-1);
 }
 
+function gotalkHowClick(){
+    $('.talkcontent_left').css('color','#1EBF95')
+    $('.talkcontent_right').css('color','#333')
+    $('#talkhow').show()
+    $('#talknew').hide()
+}
+function gotalkNewClick(){
+    $('.talkcontent_left').css('color','#333')
+    $('.talkcontent_right').css('color','#1EBF95')
+    $('#talkhow').hide()
+    $('#talknew').show()    
+}
 //评论提交事件
 function speaksendClick(){
     //提交代码写在这里,提交成功后跳转到评论页
