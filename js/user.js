9db6d1c602888f6171872b350ed4ed969b90a115
李棠寅 <379463001@qq.com>
2018-06-20 07:31:13 +0800

    Update user.js

--- js/user.js
+++ js/user.js
@@ -5,3 +5,7 @@ light.otherheader.createHeadeer('个人中心')
 function goPageClick(test){
     window.location.href=  test + ".html"
 }
+//初始化底部导航
+$('.footer_list').eq(4).find('span').css('color','#25C58D')
+$('.no_act').eq(4).show().css('display','block')
+$('.act_img').eq(4).hide();
