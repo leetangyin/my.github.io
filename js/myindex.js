97c4728113daaf9a29aada60dd439caacd901034
李棠寅 <379463001@qq.com>
2018-06-20 12:18:37 +0800

    Add files via upload

--- js/myindex.js
+++ js/myindex.js
@@ -1,2 +1,7 @@
 //初始化头部
 light.otherheader.createHeadeer('个人主页')
+
+//赛事详情页面跳转
+function goDetailsPageClick (url){
+    window.location.href = url + ".html" 
+}
--- js/mypk.js
+++ js/mypk.js
@@ -1,2 +1,6 @@
 //初始化头部
 light.otherheader.createHeadeer('我的赛事')
+//赛事详情页面跳转
+function goDetailsPageClick (url){
+    window.location.href = url + ".html" 
+}
