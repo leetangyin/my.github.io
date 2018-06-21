67cb684872f34de1942368ea50135e191c051061
李棠寅 <379463001@qq.com>
2018-06-22 07:39:02 +0800

    Add files via upload

--- js/goods.js
+++ js/goods.js
@@ -1,2 +1,14 @@
 //初始化头部
 light.otherheader.createHeadeer('商品详情')
+//兑换取消点击事件
+function cancelClick(){
+    $('#my_page_buttom').hide();
+}
+//兑换成功点击事件
+function successfulClick(){
+    $('#my_page_buttom').hide();
+    $('#toast').show();
+    setTimeout(function() {
+        $('#toast').hide();
+    }, 1000);
+}
