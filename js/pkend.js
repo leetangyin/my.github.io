b0257718683e2012dd4a24c5e9cb0f2bd3242362
李棠寅 <379463001@qq.com>
2018-07-07 14:29:57 +0800

    Add files via upload

--- js/pkend.js
+++ js/pkend.js
@@ -1,4 +1,10 @@
 //返回上一级事件
 function goBackClick(){
+    console.log('lee')
     window.history.go(-1);
 }
+//初始化底部导航
+$('.footer_list').eq(2).find('span').css('color','#25C58D')
+$('.no_act').eq(2).show().css('display','block');
+$('.act_img').eq(2).hide();
+
